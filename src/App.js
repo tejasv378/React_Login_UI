af96cf5c5e3153157965a8a7de011aaf27ddb2a3
tejasv378 <venkatesw@hartford.edu>
2023-07-16 20:09:43 -0400

    Defined user role routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Login from "./Login";
 import Admin from "./Admin";
@@ -9,7 +9,12 @@ import Resident from "./Resident";
 const App = () => {
   return (
     <Router>
-      
+      <Routes>
+        <Route exact path="/" element={<Login/>}> </Route>
+        <Route exact path="/admin" element={<Admin/>}> </Route>
+        <Route exact path="/client" element={<Client/>}> </Route>
+        <Route exact path="/resident" element={<Resident/>}> </Route>
+      </Routes>
     </Router>
   );
 };
